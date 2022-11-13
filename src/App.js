f0e99b7f87b200b3debe41978f283d1d54b33615
YingfeiHong01 <89432543+YingfeiHong01@users.noreply.github.com>
2022-11-12 21:41:39 -0500

    testimonial

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from "./scenes/Contact";
 import Footer from "./scenes/Footer";
 import useMediaQuery from "./hooks/useMediaQuery";
 import { useEffect, useState } from "react";
-import Testimonials from "./scenes/Testimonials";
+import Internships from "./scenes/Internships";
 import { motion } from "framer-motion";
 
 function App() {
@@ -75,9 +75,9 @@ function App() {
         <motion.div
           margin="0 0 -200px 0"
           amount="all"
-          onViewportEnter={() => setSelectedPage("testimonials")}
+          onViewportEnter={() => setSelectedPage("internships")}
         >
-          <Testimonials />
+          <Internships />
         </motion.div>
       </div>
       <LineGradient />
